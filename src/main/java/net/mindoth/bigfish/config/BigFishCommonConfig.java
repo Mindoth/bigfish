7253e05d7156ff6adcf9a3d76c82cfe616ff6471
Mindoth <Mindoth@gmail.com>
2025-10-27 13:02:39 +0200

    Updated repository

--- src/main/java/net/mindoth/bigfish/config/BigFishCommonConfig.java
+++ src/main/java/net/mindoth/bigfish/config/BigFishCommonConfig.java
@@ -5,14 +5,33 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class BigFishCommonConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
-
-    public static final ForgeConfigSpec.ConfigValue<Double> SPECIAL_FISH_CHANCE;
+    public static final ForgeConfigSpec.ConfigValue<Integer> ANGLERFISH_BOOST_POWER;
+    public static final ForgeConfigSpec.ConfigValue<Integer> ANGLERFISH_BOOST_TIME;
+    public static final ForgeConfigSpec.ConfigValue<Integer> EEL_HUNGER_MIN;
+    public static final ForgeConfigSpec.ConfigValue<Double> EEL_SATURARION_MIN;
+    public static final ForgeConfigSpec.ConfigValue<Integer> EEL_HUNGER_MAX;
+    public static final ForgeConfigSpec.ConfigValue<Double> EEL_SATURARION_MAX;
 
     static {
         BUILDER.push("Configs for Big Fish");
 
-        SPECIAL_FISH_CHANCE = BUILDER.comment("The chance of fishing up a fish from this mod instead of another fish. Default = 0.5 = 50%")
-                .defineInRange("Chance", 0.5, 0.0, 1.0);
+        ANGLERFISH_BOOST_POWER = BUILDER.comment("Anglerfish health boost amplifier (Default = 0)")
+                .define("Anglerfish boost power", 0);
+
+        ANGLERFISH_BOOST_TIME = BUILDER.comment("Amount of time for health boost given by anglerfish in ticks (Default = 1800)")
+                .define("Anglerfish boost time", 1800);
+
+        EEL_HUNGER_MIN = BUILDER.comment("Minimum hunger restored by cooked slimy eel (Default = 1)")
+                .define("Min eel hunger", 1);
+
+        EEL_SATURARION_MIN = BUILDER.comment("Minimum saturation restored by cooked slimy eel (Default = 0.1)")
+                .define("Min eel saturation", 0.1);
+
+        EEL_HUNGER_MAX = BUILDER.comment("Maximum hunger restored by cooked slimy eel (Default = 10)")
+                .define("Max eel hunger", 10);
+
+        EEL_SATURARION_MAX = BUILDER.comment("Maximum saturation restored by cooked slimy eel (Default = 1.0)")
+                .define("Max eel saturation", 1.0);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
--- src/main/java/net/mindoth/bigfish/event/CommonEvents.java
+++ src/main/java/net/mindoth/bigfish/event/CommonEvents.java
@@ -2,21 +2,51 @@ package net.mindoth.bigfish.event;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.mindoth.bigfish.BigFish;
+import net.mindoth.bigfish.config.BigFishCommonConfig;
 import net.mindoth.bigfish.registry.BigFishItems;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.List;
+import java.util.Random;
 
 
 @Mod.EventBusSubscriber(modid = BigFish.MOD_ID)
 public class CommonEvents {
+
+    @SubscribeEvent
+    public static void onPlayerAte(final LivingEntityUseItemEvent.Finish event) {
+        if ( event.getEntity() instanceof Player) {
+            Player player = (Player)event.getEntity();
+            FoodData foodData = player.getFoodData();
+            if ( !player.level().isClientSide ) {
+                if ( event.getItem().getItem().equals(BigFishItems.COOKED_SLIMY_EEL.get()) ) {
+                    int addedFood = (int)(BigFishCommonConfig.EEL_HUNGER_MIN.get() + Math.random() * (BigFishCommonConfig.EEL_HUNGER_MAX.get() - BigFishCommonConfig.EEL_HUNGER_MIN.get()));
+                    foodData.setFoodLevel(foodData.getFoodLevel() + addedFood);
+                    float addedSaturation = (float)(BigFishCommonConfig.EEL_SATURARION_MIN.get() + Math.random() * (BigFishCommonConfig.EEL_SATURARION_MAX.get() - BigFishCommonConfig.EEL_SATURARION_MIN.get()));
+                    foodData.setSaturation(foodData.getSaturationLevel() + addedSaturation);
+                }
+                if ( event.getItem().getItem().equals(BigFishItems.COOKED_ANGLERFISH.get()) ) {
+                    player.addEffect(new MobEffectInstance(MobEffects.HEALTH_BOOST, BigFishCommonConfig.ANGLERFISH_BOOST_TIME.get(), BigFishCommonConfig.ANGLERFISH_BOOST_POWER.get()));
+                }
+            }
+        }
+    }
 
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent event) {
@@ -26,7 +56,7 @@ public class CommonEvents {
             int villagerLevel = 5;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.DARK_CRAB.get(), 4),
+                    new ItemStack(BigFishItems.RAW_DARK_CRAB.get(), 4),
                     stack,12,30,0.05F));
         }
         if(event.getType() == VillagerProfession.FISHERMAN) {
@@ -35,7 +65,7 @@ public class CommonEvents {
             int villagerLevel = 5;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.MANTARAY.get(), 4),
+                    new ItemStack(BigFishItems.RAW_MANTARAY.get(), 4),
                     stack,12,30,0.05F));
         }
 
@@ -45,7 +75,7 @@ public class CommonEvents {
             int villagerLevel = 4;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.SHARK.get(), 6),
+                    new ItemStack(BigFishItems.RAW_SHARK.get(), 6),
                     stack,12,30,0.05F));
         }
         if(event.getType() == VillagerProfession.FISHERMAN) {
@@ -54,7 +84,7 @@ public class CommonEvents {
             int villagerLevel = 4;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.ANGLERFISH.get(), 6),
+                    new ItemStack(BigFishItems.RAW_ANGLERFISH.get(), 6),
                     stack,12,30,0.05F));
         }
 
@@ -64,7 +94,7 @@ public class CommonEvents {
             int villagerLevel = 3;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.MONKFISH.get(), 13),
+                    new ItemStack(BigFishItems.RAW_MONKFISH.get(), 13),
                     stack,16,20,0.05F));
         }
         if(event.getType() == VillagerProfession.FISHERMAN) {
@@ -73,7 +103,7 @@ public class CommonEvents {
             int villagerLevel = 3;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.SWORDFISH.get(), 13),
+                    new ItemStack(BigFishItems.RAW_SWORDFISH.get(), 13),
                     stack,16,20,0.05F));
         }
 
@@ -83,7 +113,7 @@ public class CommonEvents {
             int villagerLevel = 2;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.LOBSTER.get(), 15),
+                    new ItemStack(BigFishItems.RAW_LOBSTER.get(), 15),
                     stack,16,10,0.05F));
         }
         if(event.getType() == VillagerProfession.FISHERMAN) {
@@ -92,8 +122,17 @@ public class CommonEvents {
             int villagerLevel = 2;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                    new ItemStack(BigFishItems.SLIMY_EEL.get(), 15),
+                    new ItemStack(BigFishItems.RAW_SLIMY_EEL.get(), 15),
                     stack,16,10,0.05F));
+        }
+        if(event.getType() == VillagerProfession.FISHERMAN) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 2);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(BigFishItems.RAW_SHRIMPS.get(), 15),
+                    stack,16,5,0.05F));
         }
     }
 }
